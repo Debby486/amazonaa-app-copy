8a8400f824f722f1d8446ee59546e3e21ea5aa88
Deborah Balogun <balogundeborah28@gmail.com>
2020-12-29 05:16:39 -0800

    sorted cors

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,10 +3,12 @@ import axios from 'axios';
 import Product from '../components/Product';
 
 export default function HomeScreen() {
+  const serviceBaseUrl = 'https://amazonaa-app-copy.herokuapp.com/'
+
   const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () =>{
-      const { data }  = await axios.get('/api/products');
+      const { data }  = await axios.get(serviceBaseUrl + '/api/products' );
       setProducts(data);
     };
     fetchData();
