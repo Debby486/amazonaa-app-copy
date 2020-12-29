85ffa4ba0450a19c18c15353984512f56e57e275
Deborah Balogun <balogundeborah28@gmail.com>
2020-12-29 07:21:38 -0800

    updated backend

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,17 @@ import cors from 'cors'
  
 const app = express();
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("../frontend/build"));
+  }
+
 app.use(cors());
 
 app.get('/api/products',cors(), (req, res) => {
     res.send(data.products);
 });
 
-app.get('/', cors(), (req, res) => {
+app.get('/api', cors(), (req, res) => {
     res.send('Server is ready');
 });
 const port = process.env.PORT || 5000;
